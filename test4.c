9b5fa5bd88bcd774819e5be0ddbdd3507c8dae2a
Peter Robinson <robinson08@MacBook-Pro.local>
2017-12-14 16:02:13 -0700

    C code change

--- test4.c
+++ test4.c
@@ -5,8 +5,10 @@
 int main() {
 
     char *str[256];
+    char str2[256];
 
     strcpy(str, "This is a test");
+    strncpy(str2, "This is a test", sizeof("This is a test"));
 
     printf("\n%s    Size= %lu\n", str, sizeof(str));
 
